3321112485fdc496455efc6f21901ed076cc276b
Gustavo Gonçalves <144861221+GustavoGSH@users.noreply.github.com>
2023-09-13 19:26:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+/*if (1 === 2){ //false
+    console.log("Os números são iguais")
+} else { //true
+    console.log('Os números não são iguais')
+} //R= ira imprimir o else */
+
+/*
+const numero1 = 43
+const numero2 = 34
+
+if (numero1 > numero2){ 
+    console.log('O número 1 é maior que o número 2') //R= é maior
+} else if (numero1 < numero2){
+    console.log('O número 1 é menor que o número 2')
+} else{
+    console.log('O número 1 é igual ao número 2') 
+} */
+
+/*const pokemonInicial = 'Squirtle'
+
+if (pokemonInicial === 'bulbasaur'){
+    console.log('Seu pokemon inicial é tipo planta e veneno')
+} else if (pokemonInicial === 'charmander'){
+    console.log('Seu pokemon inicial é tipo fogo')
+} else if (pokemonInicial === 'Squirtle'){ //R=é tipo água
+    console.log('Seu pokemon inicial é tipo água')
+} else {
+    console.log('Você não escolheu nenhum')
+} */
+
+/*let pokemon = 'Squirtle'
+    switch (pokemon) {
+        case 'Bulbasauro':
+            console.log('Seu pokemon é do tipo planta e veneno')
+            break;
+    case 'Charmander':
+        console.log('Seu pokemon é do tipo Fogo')
+        break;
+    case 'Squirtle':
+        console.log('Seu pokemon é tipo água') //R=é tipo água
+        default:
+            break;
+    } */
+
+/*let concluiuEnsinoMedio = false;
+let maiorDeIdade = 16 ;
+let cursandoFaculdade = false;
+
+if (concluiuEnsinoMedio === false && maiorDeIdade >= 18 && cursandoFaculdade === false) {
+  console.log("Você pode estudar em uma faculdade");
+} else {
+  console.log("Você não pode estudar em uma faculdade"); R=Imprimiu essa daqui
+}*/
+
+/*let concluiuEnsinoMedio = prompt("Você concluiu o ensino médio?");
+let maiorDeIdade = Number(prompt("Digite sua idade:"));
+let cursandoFaculdade = prompt("Você esta cursando uma faculdade?");
+
+function verificaFacul(concluiuEnsinoMedio, maiorDeIdade, cursandoFaculdade) {
+  if (
+    concluiuEnsinoMedio === "sim" &&
+    maiorDeIdade >= 18 &&
+    cursandoFaculdade === "não"
+  ) {
+    console.log("Você pode estudar em uma faculdade");
+  } else {
+    console.log("Você não pode estudar em uma faculdade");
+  }
+}
+verificaFacul(concluiuEnsinoMedio, maiorDeIdade, cursandoFaculdade);*/
