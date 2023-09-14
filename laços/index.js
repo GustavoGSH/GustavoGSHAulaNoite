53dcdfa7a6a72b22dc87f7a4da7fe27226317400
Gustavo Gonçalves <144861221+GustavoGSH@users.noreply.github.com>
2023-09-13 21:29:24 -0300

    Add files via upload

---
+++ laços/index.js
@@ -0,0 +1,60 @@
+//Exemplo 1: Imprimindo alguns números
+/* let i = 0; // --> começo
+//i = Voltas
+
+//while = enquanto
+while (i < 10) { //condição de continuação
+
+  console.log(i); //ação repetida
+
+  i++; //--> incremento
+}
+
+
+let voltas = 0;
+
+while (voltas < 20) {
+  console.log(`O ciclista andou ${voltas} voltas`);
+
+  voltas++;
+}
+
+
+/*for (let voltas = 0; voltas < 10; voltas++) {
+  console.log(`O ciclista andou ${voltas} voltas`);
+}
+
+const numeros = [41, 43, 60, 69, 78, 100, 23, 1, 4]
+
+for (let i = 0; i < 5; i++){
+    const elementos = numeros[i]
+    console.log(elementos) //R= 41 43 60 69 78
+}
+
+const numeros = [13, 22, 69, 31, 53]
+
+for (let numero of numeros){
+    console.log(numero) //R= 13 22 69 31 53
+}
+
+//exercicio 1:
+let par = 0;
+
+while (par <= 10) {
+  if (par % 2 === 0) {
+    console.log(par);
+  }
+  par++;  //R=0 2 4 6 8 10
+} 
+
+//exercicio 2:
+let bomba = 10
+
+while (bomba >= 0){
+    if(bomba > 0){
+     console.log(`tic tac ${bomba}`)
+    } else {
+      console.log(`boooom! ${bomba}`)
+    }
+    bomba--  //R= tic tac 10, tic tac 9, tic tac 8, tic tac 7, tic tac 6, tic tac 5, tic tac 4, tic tac 3, tic tac 2, tic tac 1, boooom!! 0
+} */
