d00050d397cbf57a6733d11b848a3271b44e1055
Gustavo Gonçalves <144861221+GustavoGSH@users.noreply.github.com>
2023-09-18 19:27:32 -0300

    Add files via upload

---
+++ testes/index.js
@@ -0,0 +1,25 @@
+let suaNacionalidade = 'Brasil'
+
+switch (suaNacionalidade) {
+  case "Brasil":
+    console.log("Você é brasileiro!");
+    break;
+  case "Estados unidos":
+    console.log("Você é americano!");
+    break;
+  case "Japão":
+    console.log("Você é japonês!");
+    break;
+  case "Canadá":
+    console.log("Você é canadense!");
+    break;
+  case "Espanha":
+    console.log("Você é espanhol!");
+    break;
+  case "China":
+    console.log("Você é chinês");
+    break;
+  default:
+    console.log("Sua nacionalida não esta cadastrada no site por enquanto!");
+    break;
+}
